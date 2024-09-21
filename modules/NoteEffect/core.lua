c6c413aec2382f42d54966b4fbd1dc07ed7d3871
WickyPlays <nguyenthienbao8601@gmail.com>
2024-09-21 23:42:43 +0700

    2.4.0 Update

--- modules/NoteEffect/core.lua
+++ modules/NoteEffect/core.lua
@@ -5,14 +5,14 @@ execute.active = true
 
 local ALPHA = nil
 local MODE = nil
-local NORMAL_NOTE, LONG_NOTE, FUZZY_NOTE = nil, nil, nil
+local NORMAL_NOTE, LONG_NOTE, FUZZY_NOTE, FUZZY_LONG_NOTE, LONG_NOTE_HOLD = nil, nil, nil, nil, nil
 
 local UnityEngine = CS.UnityEngine
 local Vector3 = UnityEngine.Vector3
 local GameObject = UnityEngine.GameObject
 local EffectPoolObj = nil
 
-local _poolY, _poolG, _poolB
+local _poolY, _poolG, _poolB, _poolGLong, _poolBLong = nil, nil, nil, nil, nil
 
 --=============================
 --         POOLING
@@ -22,12 +22,12 @@ local function createEffect(prefab)
 	return {
 		prefab = prefab,
 		active = false,
-		ticks = 120
+		ticks = 120,
+		unlimited = false,
 	}
 end
 
 local function createEffectPool(_prefab, maxEffects)
-
 	local eff = {
 		prefab = _prefab,
 		maxEffects = maxEffects,
@@ -41,8 +41,6 @@ local function createEffectPool(_prefab, maxEffects)
 			s.transform:SetParent(EffectPoolObj.transform, false)
 			s.transform.localPosition = Vector3(0, 5000, 0)
 			s.transform.localScale = Vector3(1.5, 1, 1)
-			local particleRenderer = s:GetComponent(typeof(CS.UnityEngine.ParticleSystemRenderer))
-			particleRenderer.material.color = util.ColorRGBA(255, 255, 255, ALPHA)
 
 			s:SetActive(false)
 			table.insert(eff.effects, createEffect(s))
@@ -58,7 +56,7 @@ local function createEffectPool(_prefab, maxEffects)
 		return true
 	end
 
-	eff.retrieve = function (pos)
+	eff.retrieve = function(pos)
 		if eff.allEffectsActive() then
 			eff.initializePool()
 		end
@@ -67,7 +65,7 @@ local function createEffectPool(_prefab, maxEffects)
 				effect.prefab:SetActive(true)
 				effect.active = true
 				effect.ticks = 120
-	
+
 				effect.prefab.transform.localPosition = pos
 				return effect
 			end
@@ -89,10 +87,12 @@ local function createEffectPool(_prefab, maxEffects)
 
 	eff.scan = function()
 		for _, effect in ipairs(eff.effects) do
-			if effect.active and effect.ticks > 0 then
-				effect.ticks = effect.ticks - 1
-				if effect.ticks <= 0 then
-					eff.releaseEffect(effect)
+			if not effect.unlimited then
+				if effect.active and effect.ticks > 0 then
+					effect.ticks = effect.ticks - 1
+					if effect.ticks <= 0 then
+						eff.releaseEffect(effect)
+					end
 				end
 			end
 		end
@@ -108,7 +108,7 @@ local function createEffectPool(_prefab, maxEffects)
 			effect.active = false
 			effect.ticks = 120
 		end
-	
+
 		if (eff.prefab ~= nil) then
 			eff.prefab = nil
 		end
@@ -131,12 +131,12 @@ local function calculateValue(lane)
 end
 
 execute.onloaded = function()
-
 	MODE = execute.GetOption("MODE")
-	ALPHA = execute.GetOption("ALPHA")
 	NORMAL_NOTE = execute.GetOption("NORMAL_NOTE")
 	LONG_NOTE = execute.GetOption("LONG_NOTE")
 	FUZZY_NOTE = execute.GetOption("FUZZY_NOTE")
+	FUZZY_LONG_NOTE_HOLD = execute.GetOption("FUZZY_LONG_NOTE_HOLD")
+	LONG_NOTE_HOLD = execute.GetOption("LONG_NOTE_HOLD")
 
 	local hash = execute.LoadAssetBundle("G_NoteEffect/" .. MODE .. '/' .. util.GetPlatformPath() .. "hit")
 
@@ -145,6 +145,8 @@ execute.onloaded = function()
 	local _hitYellow = util.LoadObjectHash(hash, NORMAL_NOTE)
 	local _hitGreen = util.LoadObjectHash(hash, FUZZY_NOTE)
 	local _hitBlue = util.LoadObjectHash(hash, LONG_NOTE)
+	local _hitGreenLong = util.LoadObjectHash(hash, FUZZY_LONG_NOTE_HOLD)
+	local _hitBlueLong = util.LoadObjectHash(hash, LONG_NOTE_HOLD)
 
 	_poolY = createEffectPool(_hitYellow, 2)
 	_poolY.initializePool()
@@ -152,6 +154,12 @@ execute.onloaded = function()
 	_poolG.initializePool()
 	_poolB = createEffectPool(_hitBlue, 2)
 	_poolB.initializePool()
+
+	_poolGLong = createEffectPool(_hitGreenLong, 2)
+	_poolGLong.initializePool()
+
+	_poolBLong = createEffectPool(_hitBlueLong, 2)
+	_poolBLong.initializePool()
 
 	GAMESTATE:ChangeJudgeEffect(0)
 end
@@ -166,6 +174,42 @@ execute.onHitNote = function(id, lane, noteType, judgeType, isAttack)
 	end
 end
 
+local longHandler = {}
+local longIndex = 0
+
+local function removeLongHandler(index)
+	for i = 1, #longHandler do
+		if longHandler[i].index == index then
+			table.remove(longHandler, i)
+			return
+		end
+	end
+end
+
+execute.onSpawnLong = function(longController)
+	local noteEffectObj = nil
+	local type = longController.LongType
+
+	if type == CS.LongType.Long then
+		noteEffectObj = _poolBLong.retrieve(Vector3(-100, 0, 0))
+	elseif type == CS.LongType.FuzzyLong then
+		noteEffectObj = _poolGLong.retrieve(Vector3(-100, 0, 0))
+	else
+		return
+	end
+
+	noteEffectObj.unlimited = true
+	local newLongHandler = {
+		index = longIndex,
+		active = false,
+		longController = longController,
+		noteEffect = noteEffectObj
+	}
+
+	table.insert(longHandler, newLongHandler)
+	longIndex = longIndex + 1
+end
+
 execute.update = function(e)
 	if _poolY then
 		_poolY.scan()
@@ -177,6 +221,35 @@ execute.update = function(e)
 
 	if _poolB then
 		_poolB.scan()
+	end
+
+	if _poolGLong and _poolBLong then
+		_poolGLong.scan()
+		_poolBLong.scan()
+
+		for _, longNote in ipairs(longHandler) do
+			if not longNote.active and not longNote.longController.IsReleased then
+				if longNote.longController.CurrentPosX >= -2 and longNote.longController.CurrentPosX <= 2 then
+					longNote.active = true
+				end
+			else
+				longNote.noteEffect.prefab.transform.localPosition = Vector3(longNote.longController.CurrentPosX, 0, 0)
+
+				if longNote.longController.IsReleased then
+					longNote.active = false
+
+					local type = longNote.longController.LongType
+
+					if type == CS.LongType.Long then
+						_poolBLong.releaseEffect(longNote.noteEffect)
+						removeLongHandler(longNote.index)
+					elseif type == CS.LongType.FuzzyLong then
+						_poolGLong.releaseEffect(longNote.noteEffect)
+						removeLongHandler(longNote.index)
+					end
+				end
+			end
+		end
 	end
 end
 
--- modules/SubtitleLyric/core.lua
+++ modules/SubtitleLyric/core.lua
@@ -1,11 +1,10 @@
---著者: Wicky
-
 local File = CS.System.IO.File
 local Directory = CS.System.IO.Directory
 local UnityEngine = CS.UnityEngine
 local GameObject = UnityEngine.GameObject
 local Vector2 = UnityEngine.Vector2
-local Canvas = UnityEngine.Canvas
+
+local _Lyric1Text, _Lyric2Text
 
 local execute = {}
 execute.active = true
@@ -37,10 +36,11 @@ local function parseSubtitle()
     return (tonumber(hours) * 3600 + tonumber(minutes) * 60 + tonumber(seconds)) * 1000 + tonumber(milliseconds)
   end
 
-  for i, line in ipairs(lines) do
+  local i = 1
+  while i <= #lines do
     local lyric = {}
 
-    local id = tonumber(line)
+    local id = tonumber(lines[i])
 
     if id ~= nil then
       lyric.id = id
@@ -48,40 +48,60 @@ local function parseSubtitle()
       local timecode = lines[i + 1]
       if timecode ~= nil then
         local startH, startM, startS, startMs, endH, endM, endS, endMs = timecode:match(
-        "(%d+):(%d+):(%d+),(%d+)%s%-%-%>%s(%d+):(%d+):(%d+),(%d+)")
+          "(%d+):(%d+):(%d+),(%d+)%s%-%-%>%s(%d+):(%d+):(%d+),(%d+)")
         lyric.startTime = timeToMs(startH, startM, startS, startMs)
         lyric.endTime = timeToMs(endH, endM, endS, endMs)
       end
 
-      local lyricLine = lines[i + 2]
-      if lyricLine ~= nil then
-        lyric.lyricLine = lyricLine
+      lyric.lyricLine1 = lines[i + 2] or ""  -- First lyric line
+
+      -- Check for a second lyric line under the same timestamp
+      local nextLine = lines[i + 3]
+      if nextLine and nextLine:match("^%d+$") == nil then
+        lyric.lyricLine2 = nextLine
+        i = i + 1 -- Skip the second lyric line in the loop
+      else
+        lyric.lyricLine2 = "" -- No second line
       end
 
       table.insert(lyrics, lyric)
 
-      i = i + 2
+      i = i + 2 -- Move to the next block
     end
+    i = i + 1
   end
 
   return lyrics
 end
 
 execute.onloaded = function(e)
-	parseSubtitle()
+  parseSubtitle()
 
   local canvas = util.GetCanvas()
-  local LyricJP = GameObject("LyricJP")
-  LyricJP.gameObject.transform:SetParent(canvas.transform, false)
-  LyricJP:AddComponent(typeof(UnityEngine.CanvasRenderer))
-  _LyricJPText = LyricJP:AddComponent(typeof(UnityEngine.UI.Text))
-  LyricJP.transform.anchorMin = Vector2(0.5, 1)
-  LyricJP.transform.anchorMax = Vector2(0.5, 1)
-  LyricJP.transform.pivot = Vector2(0.5, 4.7)
-  LyricJP.transform.sizeDelta = Vector2(util.GetScreenWidth(), 260)
-  _LyricJPText.font = util.GetFontJP()
-  _LyricJPText.fontSize = 36
-  _LyricJPText.alignment = UnityEngine.TextAnchor.UpperCenter
+
+  local Lyric1 = GameObject("Lyric1")
+  Lyric1.gameObject.transform:SetParent(canvas.transform, false)
+  Lyric1:AddComponent(typeof(UnityEngine.CanvasRenderer))
+  _Lyric1Text = Lyric1:AddComponent(typeof(UnityEngine.UI.Text))
+  Lyric1.transform.anchorMin = Vector2(0.5, 1)
+  Lyric1.transform.anchorMax = Vector2(0.5, 1)
+  Lyric1.transform.pivot = Vector2(0.5, 4.65)
+  Lyric1.transform.sizeDelta = Vector2(util.GetScreenWidth(), 260)
+  _Lyric1Text.font = util.GetFontJP()
+  _Lyric1Text.fontSize = 36
+  _Lyric1Text.alignment = UnityEngine.TextAnchor.UpperCenter
+
+  local Lyric2 = GameObject("Lyric2")
+  Lyric2.gameObject.transform:SetParent(canvas.transform, false)
+  Lyric2:AddComponent(typeof(UnityEngine.CanvasRenderer))
+  _Lyric2Text = Lyric2:AddComponent(typeof(UnityEngine.UI.Text))
+  Lyric2.transform.anchorMin = Vector2(0.5, 1)
+  Lyric2.transform.anchorMax = Vector2(0.5, 1)
+  Lyric2.transform.pivot = Vector2(0.5, 4.85)
+  Lyric2.transform.sizeDelta = Vector2(util.GetScreenWidth(), 260)
+  _Lyric2Text.font = util.GetFontJP()
+  _Lyric2Text.fontSize = 34
+  _Lyric2Text.alignment = UnityEngine.TextAnchor.UpperCenter
 end
 
 local currLyricIndex = 1
@@ -92,14 +112,17 @@ execute.update = function()
 
   if currLyric then
     if currentTimeMs >= currLyric.startTime and currentTimeMs <= currLyric.endTime then
-      _LyricJPText.text = currLyric.lyricLine
+      _Lyric1Text.text = currLyric.lyricLine1
+      _Lyric2Text.text = currLyric.lyricLine2
     elseif currentTimeMs > currLyric.endTime then
       currLyricIndex = currLyricIndex + 1
     else
-      _LyricJPText.text = ""
+      _Lyric1Text.text = ""
+      _Lyric2Text.text = ""
     end
   else
-    _LyricJPText.text = ""
+    _Lyric1Text.text = ""
+    _Lyric2Text.text = ""
   end
 end
 
